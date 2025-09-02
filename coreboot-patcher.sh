69701381dc1fe8c88aee3bad7a2fa9e67e5f288a
cruzy22k <zakisullivan63@gmail.com>
2025-09-02 22:10:24 +1000

    final

--- coreboot-patcher.sh
+++ coreboot-patcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail # fail on any error
-SCRIPT_VER=3.03
+SCRIPT_VER=3.04
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" 
 DATA_DIR="$SCRIPT_DIR"
 TOOLS_DIR="$(mktemp -d "${SCRIPT_DIR}/.tools.XXXXXXXXXX")"
@@ -24,17 +24,14 @@ downloader() {
             echo "'$name' already exists"
             return
         fi
-        
         echo "Downloading '$name'..."
         cd "$TOOLS_DIR"
-
         if ! { wget --quiet --show-progress -O "$name.tar.gz" "$url" && tar -zxf "$name.tar.gz" && chmod +x "$name"; }; then
             echo -e "\e[31mError: Failed to download or set up '$name'. Please check your internet connection.\e[0m" >&2
             exit 1
         fi
-        cd "$SCRIPT_DIR"
+        cd "$SCRIPT_DIR" # back to main
         echo -e "\e[32mSuccessfully set up '$name'.\e[0m"
-
     }
 
     download_util "flashrom" "https://mrchromebox.tech/files/util/flashrom_ups_libpci37_20240418.tar.gz"
@@ -42,8 +39,10 @@ downloader() {
     download_util "gbb_utility" "https://mrchromebox.tech/files/util/gbb_utility.tar.gz"
     
     echo -e "\n\e[32mAll required utilities are ready.\e[0m"
-    sleep 1
     clear
+    sleep 1
+    clear # why tf won't it clear?
+    
 }
 print_help() {
     cat <<EOF
@@ -129,14 +128,14 @@ main() {
     echo "Your new rom 'modified_coreboot.rom' has been successfully injected!."
     echo -e "\nThe next step is to flash the modified ROM."
     echo "Proceeding with flashing. Ensure this is the correct machine!"
-    echo -e "Flash your custom firmware:"
-    echo ""
-    echo "AMD devices: sudo ./flashrom -p internal -w coreboot.rom"
-    echo "Intel devices: sudo ./flashrom -p internal --ifd -i bios -w coreboot.rom -N"
-    
+    echo -e "To flash your custom firmware run the following commands: "
+
+    echo -e "\e[32mAMD devices: sudo ./flashrom -p internal -w coreboot.rom\e[0m"
+    echo -e "\e[32mIntel devices: sudo ./flashrom -p internal --ifd -i bios -w coreboot.rom -N\e[0m"
     echo ""
     echo -e "Made with ♡ by Cruzy22k" 
     echo ":3"
+    # cleanup
     mv coreboot.rom modified_coreboot.rom
     sudo rm vpd.bin && sudo rm hwid.txt
 }
